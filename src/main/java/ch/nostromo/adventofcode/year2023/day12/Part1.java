05ca4d21eceaaa8f6f86d5fbce60badc59a2ef09
Bernhard von Gunten <bvg@nostromo.ch>
2023-12-12 23:35:47 +0100

    2023 Day 12

---
+++ src/main/java/ch/nostromo/adventofcode/year2023/day12/Part1.java
@@ -0,0 +1,166 @@
+package ch.nostromo.adventofcode.year2023.day12;
+
+import ch.nostromo.adventofcode.BasePuzzle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * --- Day 12: Hot Springs ---
+ * You finally reach the hot springs! You can see steam rising from secluded areas attached to the primary, ornate building.
+ * <p>
+ * As you turn to enter, the researcher stops you. "Wait - I thought you were looking for the hot springs, weren't you?" You indicate that this definitely looks like hot springs to you.
+ * <p>
+ * "Oh, sorry, common mistake! This is actually the onsen! The hot springs are next door."
+ * <p>
+ * You look in the direction the researcher is pointing and suddenly notice the massive metal helixes towering overhead. "This way!"
+ * <p>
+ * It only takes you a few more steps to reach the main gate of the massive fenced-off area containing the springs. You go through the gate and into a small administrative building.
+ * <p>
+ * "Hello! What brings you to the hot springs today? Sorry they're not very hot right now; we're having a lava shortage at the moment." You ask about the missing machine parts for Desert Island.
+ * <p>
+ * "Oh, all of Gear Island is currently offline! Nothing is being manufactured at the moment, not until we get more lava to heat our forges. And our springs. The springs aren't very springy unless they're hot!"
+ * <p>
+ * "Say, could you go up and see why the lava stopped flowing? The springs are too cold for normal operation, but we should be able to find one springy enough to launch you up there!"
+ * <p>
+ * There's just one problem - many of the springs have fallen into disrepair, so they're not actually sure which springs would even be safe to use! Worse yet, their condition records of which springs are damaged (your puzzle input) are also damaged! You'll need to help them repair the damaged records.
+ * <p>
+ * In the giant field just outside, the springs are arranged into rows. For each row, the condition records show every spring and whether it is operational (.) or damaged (#). This is the part of the condition records that is itself damaged; for some springs, it is simply unknown (?) whether the spring is operational or damaged.
+ * <p>
+ * However, the engineer that produced the condition records also duplicated some of this information in a different format! After the list of springs for a given row, the size of each contiguous group of damaged springs is listed in the order those groups appear in the row. This list always accounts for every damaged spring, and each number is the entire size of its contiguous group (that is, groups are always separated by at least one operational spring: #### would always be 4, never 2,2).
+ * <p>
+ * So, condition records with no unknown spring conditions might look like this:
+ * <p>
+ * #.#.### 1,1,3
+ * .#...#....###. 1,1,3
+ * .#.###.#.###### 1,3,1,6
+ * ####.#...#... 4,1,1
+ * #....######..#####. 1,6,5
+ * .###.##....# 3,2,1
+ * However, the condition records are partially damaged; some of the springs' conditions are actually unknown (?). For example:
+ * <p>
+ * ???.### 1,1,3
+ * .??..??...?##. 1,1,3
+ * ?#?#?#?#?#?#?#? 1,3,1,6
+ * ????.#...#... 4,1,1
+ * ????.######..#####. 1,6,5
+ * ?###???????? 3,2,1
+ * Equipped with this information, it is your job to figure out how many different arrangements of operational and broken springs fit the given criteria in each row.
+ * <p>
+ * In the first line (???.### 1,1,3), there is exactly one way separate groups of one, one, and three broken springs (in that order) can appear in that row: the first three unknown springs must be broken, then operational, then broken (#.#), making the whole row #.#.###.
+ * <p>
+ * The second line is more interesting: .??..??...?##. 1,1,3 could be a total of four different arrangements. The last ? must always be broken (to satisfy the final contiguous group of three broken springs), and each ?? must hide exactly one of the two broken springs. (Neither ?? could be both broken springs or they would form a single contiguous group of two; if that were true, the numbers afterward would have been 2,3 instead.) Since each ?? can either be #. or .#, there are four possible arrangements of springs.
+ * <p>
+ * The last line is actually consistent with ten different arrangements! Because the first number is 3, the first and second ? must both be . (if either were #, the first number would have to be 4 or higher). However, the remaining run of unknown spring conditions have many different ways they could hold groups of two and one broken springs:
+ * <p>
+ * ?###???????? 3,2,1
+ * .###.##.#...
+ * .###.##..#..
+ * .###.##...#.
+ * .###.##....#
+ * .###..##.#..
+ * .###..##..#.
+ * .###..##...#
+ * .###...##.#.
+ * .###...##..#
+ * .###....##.#
+ * In this example, the number of possible arrangements for each row is:
+ * <p>
+ * ???.### 1,1,3 - 1 arrangement
+ * .??..??...?##. 1,1,3 - 4 arrangements
+ * ?#?#?#?#?#?#?#? 1,3,1,6 - 1 arrangement
+ * ????.#...#... 4,1,1 - 1 arrangement
+ * ????.######..#####. 1,6,5 - 4 arrangements
+ * ?###???????? 3,2,1 - 10 arrangements
+ * Adding all of the possible arrangement counts together produces a total of 21 arrangements.
+ * <p>
+ * For each row, count all of the different arrangements of operational and broken springs that meet the given criteria. What is the sum of those counts?
+ * <p>
+ * Your puzzle answer was 7118.
+ */
+public class Part1 extends BasePuzzle {
+
+    private static final String EXPECTED_TEST_RESULT = "21";
+
+    public String solvePuzzle(List<String> input) {
+        long result = 0;
+
+        for (String line : input) {
+            String inputStr = line.split("\\s+")[0];
+
+            List<Integer> numbers = new ArrayList<>();
+            for (String number : line.split("\\s+")[1].split(",")) {
+                numbers.add(Integer.valueOf(number));
+            }
+
+            result += recursiveSearch(numbers, inputStr);
+        }
+
+        return String.valueOf(result);
+    }
+
+
+    private long recursiveSearch(List<Integer> numbers, String restLine) {
+        long result = 0;
+
+
+        if (restLine.length() == 0) {
+            if (numbers.size() == 0) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
+        if (restLine.startsWith(".")) {
+            String futureRestLine = restLine.substring(1);
+            return recursiveSearch(numbers, futureRestLine);
+        } else if (restLine.startsWith("?")) {
+            String futureRestLineDot = restLine.substring(1);
+            long resultIfEmpty = recursiveSearch(numbers, "." + futureRestLineDot);
+            long resultIfUsable = recursiveSearch(numbers, "#" + futureRestLineDot);
+            return resultIfEmpty + resultIfUsable;
+        }
+
+
+        if (numbers.size() == 0) {
+            // nope
+            return 0;
+        }
+
+
+        int nextNumber = numbers.get(0);
+        if (nextNumber > restLine.length() || !isBookable(restLine.substring(0, nextNumber))) {
+            return 0;
+        }
+
+        if (nextNumber == restLine.length()) {
+            if (numbers.size() == 1) {
+                result = 1;
+            }
+        } else if (restLine.charAt(nextNumber) == '.' || restLine.charAt(nextNumber) == '?') {
+            result = recursiveSearch(numbers.subList(1, numbers.size()), restLine.substring(nextNumber + 1));
+        }
+
+
+        return result;
+    }
+
+    private boolean isBookable(String line) {
+        for (char c : line.toCharArray()) {
+            if (c != '?' && c != '#') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Part1() {
+        super(EXPECTED_TEST_RESULT);
+    }
+
+    public static void main(String... args) {
+        new Part1().run();
+    }
+
+}
